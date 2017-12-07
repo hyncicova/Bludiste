f3e8b2ab78732a52f39a67ea6a0ea1df64e002e7
hyncicova <34132885+hyncicova@users.noreply.github.com>
2017-12-07 16:04:04 +0100

    Add files via upload

---
+++ Bludiste/Avatar.java
@@ -0,0 +1,6 @@
+
+public class Avatar {
+	
+	Knot poloha = new Knot();
+	
+}
---
+++ Bludiste/Knot.java
@@ -0,0 +1,29 @@
+
+public class Knot {
+	int X;
+	int Y;
+	
+	boolean finish = false;
+
+	
+	Knot up = new Knot();
+	Knot down = new Knot();
+	Knot left = new Knot();
+	Knot right = new Knot();
+	
+	public Knot(){
+		
+	}
+	
+	public Knot (int X, int Y, Knot up, Knot down, Knot left, Knot right){
+		
+		this.X = X;
+		this.Y = Y;
+		
+		this.up = up;
+		this.down = down;
+		this.left = left;
+		this.right = right;
+	}
+
+}
